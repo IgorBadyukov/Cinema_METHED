b9f951786eb15499baddd5589c71267e77453a15
IgorBadyukov <badyukov1806302003@gmail.com>
2021-12-08 19:33:33 +0300

    подключил файл рендера фильма недели

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import slideMenu from "./menu.js";
+import renderVideo from "./renderVideo.js";
 
 slideMenu({
     openBtn: '.header__burger-btn',
@@ -6,5 +7,7 @@ slideMenu({
     classActiveMenu: 'navigation_active', 
     closeTrigger: '.navigation__link, .navigation__close'
 });
+
+renderVideo();
 
 
